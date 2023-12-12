ce1e0e09db69b224018ad214c50ef7247d9d6407
Matthew Moniz <Matt@Fastforward.sh>
2023-12-12 15:23:45 -0500

    fixed dependency issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
-import './App.css'
+import React, {useEffect} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {FiSettings} from 'react-icons/fi';
+import {TooltipComponent} from '@syncfusion/ej2-react-popups';
+import './App.css';
 
 const App = () => {
   return (
-    <h1 className='underline text-3xl'>App</h1>
+    <div className='underline text-3xl'>
+
+        <BrowserRouter >
+        <div className='flex relative dark:bg-main-bg'>
+            <div className='fixed right-4 bottom-4' style={{zIndex:'1000'}}>
+                <TooltipComponent content="settings" position="top">
+                    <button type='button' className='text-3xl p-3 hover:drop-shadow-xl gover-bg-light-gray text-white' style={{background:'blue', borderRadius:'50%'}}>
+                        <FiSettings />
+                    </button>
+                </TooltipComponent>
+            </div>
+
+        </div>
+
+        </BrowserRouter>
+    </div>
   )
 }
 
